5069fc507101b945ce3b69a779d4272228046953
Christopher Cifra <christopher.cifra@ni.com>
2020-01-20 15:05:16 -0600

    Add a CounterService which provides the current count Add WPF UI with count display and increment button that uses the counter service Update Counter page to use the counter service

---
+++ BlazorUI/Data/CounterService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace BlazorUI.Data
+{
+    public class CounterService : INotifyPropertyChanged
+    {
+        private int _count;
+
+        public static CounterService Instance { get; } = new CounterService();
+
+        public int Count
+        {
+            get => _count;
+            set
+            {
+                _count = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
--- WPFHostApp/MainWindow.xaml.cs
+++ WPFHostApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BlazorUI.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,13 @@ namespace WPFHostApp
         public MainWindow()
         {
             InitializeComponent();
+
+            _counterText.DataContext = CounterService.Instance;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CounterService.Instance.Count += 1;
         }
     }
 }
